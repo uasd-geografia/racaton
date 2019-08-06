fcd147d22a75a8087ebb282e7eeac366b2669224
Edwin Valdimir Medina Castillo <53198363+edwin89holiday@users.noreply.github.com>
2019-08-06 10:54:42 -0400

    Update script_de_edwin.R

--- preguntas/p1-dif-param-morf-ocoa-vaca/script_de_edwin.R
+++ preguntas/p1-dif-param-morf-ocoa-vaca/script_de_edwin.R
@@ -1,17 +1,25 @@
+#paquetes 
 library(raster)
 library(rgrass7)
 library(rgdal)
 library(sf)
-getwd()     
+#fijar el directorio de trabajo
+getwd()
+#importar y asignar ratser
 raster("../compartidos/n18_w071_1arc_v3.tif")  
 mde <- raster("../compartidos/n18_w071_1arc_v3.tif")  
 mde
 mde[]
+#caracteristicas del raster
 length(mde)
 length(mde)
 min(mde[])
+max(mde[])
+#importar y asignar capa vectorial 
 cuencas <- st_read("../compartidos/kml_la_vaca_ocoa.gpkg")
+#cortar area del raster con la capa vectorial
 corte_cuenca <- crop(mde, cuencas)
-?crop
+?crop #ayuda
+#plotear el raster
 plot(corte_cuenca)
-rm(mde) 
+rm(mde) #eliminar el raster original de la data de trabajo
